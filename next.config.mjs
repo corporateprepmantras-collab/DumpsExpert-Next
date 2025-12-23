49e8eaa5bdd10276c875c1aa05ad7c944e322dc8
Yogesh832e <upadhayayyogesh832@gmail.com>
2025-12-23 08:25:10 +0530

    updates

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,23 @@ const nextConfig = {
     removeConsole: process.env.NODE_ENV === "production",
   },
 
+
+  async redirects() {
+  return [
+    {
+      source: "/:path*",
+      has: [
+        {
+          type: "host",
+          value: "prepmantras.com",
+        },
+      ],
+      destination: "https://www.prepmantras.com/:path*",
+      permanent: true,
+    },
+  ];
+},
+
   images: {
     formats: ["image/avif", "image/webp"],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
