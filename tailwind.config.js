269c64a7f9fde8e96a15e778d59f3ca231da932c
Yogesh832e <upadhayayyogesh832@gmail.com>
2026-01-28 18:07:16 +0530

    add optimized image component and skeleton loaders; update next config and dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: "class",
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "!**/*.test.{js,jsx}",
+    "!**/node_modules/**",
+  ],
   theme: {
     extend: {
       borderRadius: {
